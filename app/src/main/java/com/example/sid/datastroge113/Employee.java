8e32ec91318afb54c4a5f8f7442736fbc19f4f11
Sid498 <sidpawar619@gmail.com>
2017-08-16 17:53:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sid/datastroge113/Employee.java
@@ -0,0 +1,13 @@
+package com.example.sid.datastroge113;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by SID on 8/16/2017.
+ */
+
+public class Employee {
+    public String nameOfEmployee;
+    public String ageOfEmployee;
+    public Bitmap employeePhoto;
+}
